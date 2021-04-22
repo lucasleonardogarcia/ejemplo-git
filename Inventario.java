f97682caba09f08c17cb34c33be22be9c63cefc1
Martín Barroso <martindanielbarroso@gmail.com>
2021-04-21 21:40:19 -0300

    Create Inventario.java

---
+++ Inventario.java
@@ -0,0 +1,21 @@
+
+package ejemplo.git;
+
+
+public class Inventario {
+    private int numSolicitud;
+
+    public Inventario(int numSolicitud) {
+        this.numSolicitud = numSolicitud;
+    }
+
+    public int getNumSolicitud() {
+        return numSolicitud;
+    }
+
+    public void setNumSolicitud(int numSolicitud) {
+        this.numSolicitud = numSolicitud;
+    }
+    
+    
+}
